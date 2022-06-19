b48ceed0a268eb103e3ab93d1f956073bd85a1df
Kirill Matviiv <matviiv67@gmail.com>
2022-06-19 23:00:11 +0300

    Update weather-visualizing-script.py

--- weather_visualizing/weather-visualizing-script.py
+++ weather_visualizing/weather-visualizing-script.py
@@ -1,41 +1,33 @@
-def get_weather_data(lat=33.44, lon=-94.04, api_key=None, dt=0, city='', days_range=0):
-    import requests
-    import json
+import matplotlib.pyplot as pplt
+import pandas
+import requests
+import json
+from datetime import datetime
 
+
+def get_weather_data(lat=33.44, lon=-94.04, api_key=None, dt=0, city='', days_range=0):
     if api_key:
-        result = dict()
-        result['city'] = city
-        result['temps'] = []
+        result = {'city': city, 'temps': []}
         for i in range(days_range):
-            req = requests.get(
-                f'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&'
-                f'appid={api_key}&lang=ru&units=metric')
-            req_obj = json.loads(req.text)
-            measures = [{"dt": str(measure['dt']), "temp": str(measure['temp'])} for measure in req_obj["hourly"]]
-            result['temps'].append(measures)
+            req_obj = json.loads(requests.get(f'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}'
+                                              f'&lon={lon}&dt={dt}&appid={api_key}&lang=ru&units=metric').text)
+            result['temps'].append([{"dt": str(measure['dt']), "temp": str(measure['temp'])} for measure in req_obj["hourly"]])
             dt += 86400
         visualise_data(json.dumps(result), days_range)
 
 
 def visualise_data(json_data='', days_range=0):
     if json_data:
-        import matplotlib.pyplot as pplt
-        import pandas
-        from datetime import datetime
-
         data = pandas.read_json(json_data)
-        dates = [_d['dt'] for _d in data['temps'][0][:]]
+        dates = [d['dt'] for d in data['temps'][0]]
         city_name = data['city'].get(0)
-        dates_hours, all_temps = [], []
-
-        for i in dates:
-            dates_hours.append(datetime.utcfromtimestamp(int(i)).strftime('%H:%M'))
-
-        date1 = datetime.utcfromtimestamp(int(dates[0])).strftime('%d/%m/%Y')
-        date2 = datetime.utcfromtimestamp(int(dates[0]) + 86400 * (days_range - 1)).strftime('%d/%m/%Y')
+        dates_hours = [datetime.utcfromtimestamp(int(i)).strftime('%H:%M') for i in dates]
+        date_start = datetime.utcfromtimestamp(int(dates[0])).strftime('%d/%m/%Y')
+        date_end = datetime.utcfromtimestamp(int(dates[0]) + 86400 * (days_range - 1)).strftime('%d/%m/%Y')
+        all_temps = []
 
         pplt.figure(figsize=(15, 5))
-        pplt.title(f'График температуры в городе {city_name}\nПериод: {date1} - {date2}')
+        pplt.title(f'График температуры в городе {city_name}\nПериод: {date_start} - {date_end}')
         pplt.xlabel("Время")
         pplt.ylabel("Температура, °C")
         colors = {'g': '1 день', 'r': '2 день', 'b': '3 день', 'y': '4 день', 'm': '5 день'}
@@ -46,17 +38,17 @@ def visualise_data(json_data='', days_range=0):
         pplt.legend(loc='lower right')
         pplt.show()
 
-        avg_temps = [(i - i) for i in range(len(all_temps[0]))]
-
+        avg_temps = [0] * len(all_temps[0])
+        print(all_temps)
+        print(avg_temps)
         for i in all_temps:
             for j in range(len(i)):
                 avg_temps[j] += i[j]
-
         for i in range(len(avg_temps)):
             avg_temps[i] /= 5
 
         pplt.figure(figsize=(15, 5))
-        pplt.title(f'График средней температуры в городе {city_name}\nПериод: {date1} - {date2}')
+        pplt.title(f'График средней температуры в городе {city_name}\nПериод: {date_start} - {date_end}')
         pplt.xlabel("Время")
         pplt.ylabel("Температура, °C")
         pplt.scatter(dates_hours, avg_temps)
@@ -64,4 +56,4 @@ def visualise_data(json_data='', days_range=0):
 
 
 if __name__ == '__main__':
-    get_weather_data(55.7522, 37.6156, 'd204de47e2ed49257fc3f7cc51a8dda7', 1637712000, 'Moscow', 5)
+    get_weather_data(55.7522, 37.6156, 'd204de47e2ed49257fc3f7cc51a8dda7', 1655164800, 'Moscow', 5)
