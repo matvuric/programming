a07724bb8a4e72c14b651ebf79fb504481163097
Kirill Matviiv <matviiv67@gmail.com>
2022-10-06 13:57:22 +0300

    test

--- integration_optimization(prog7lr2)/test.py
+++ integration_optimization(prog7lr2)/test.py
@@ -1,53 +1,39 @@
-import unittest
+import pytest
 import math
 from integrate import integrate, integrate_async
 
 
-class TestIntegrate(unittest.TestCase):
-    def test_sin_async2(self):
-        self.assertEqual(integrate_async(math.sin, 0, 2, n_iter=1000), integrate(math.sin, 0, 2, n_iter=1000))
-        self.assertEqual(integrate_async(math.sin, 0, 2, n_iter=10000), integrate(math.sin, 0, 2, n_iter=10000))
-        self.assertEqual(integrate_async(math.sin, 0, 2, n_iter=100000), integrate(math.sin, 0, 2, n_iter=100000))
-        self.assertEqual(integrate_async(math.sin, 1, 20, n_iter=1000), integrate(math.sin, 1, 20, n_iter=1000))
-        self.assertEqual(integrate_async(math.sin, 1, 20, n_iter=10000), integrate(math.sin, 1, 20, n_iter=10000))
-        self.assertEqual(integrate_async(math.sin, 1, 20, n_iter=100000), integrate(math.sin, 1, 20, n_iter=100000))
-
-    def test_cos_async2(self):
-        self.assertEqual(integrate_async(math.cos, 0, 2, n_iter=1000), integrate(math.cos, 0, 2, n_iter=1000))
-        self.assertEqual(integrate_async(math.cos, 0, 2, n_iter=10000), integrate(math.cos, 0, 2, n_iter=10000))
-        self.assertEqual(integrate_async(math.cos, 0, 2, n_iter=100000), integrate(math.cos, 0, 2, n_iter=100000))
-        self.assertEqual(integrate_async(math.cos, 1, 20, n_iter=1000), integrate(math.cos, 1, 20, n_iter=1000))
-        self.assertEqual(integrate_async(math.cos, 1, 20, n_iter=10000), integrate(math.cos, 1, 20, n_iter=10000))
-        self.assertEqual(integrate_async(math.cos, 1, 20, n_iter=100000), integrate(math.cos, 1, 20, n_iter=100000))
-
-    def test_sin_async4(self):
-        self.assertEqual(integrate_async(math.sin, 0, 2, n_jobs=4, n_iter=1000),
-                         integrate(math.sin, 0, 2, n_iter=1000))
-        self.assertEqual(integrate_async(math.sin, 0, 2, n_jobs=4, n_iter=10000),
-                         integrate(math.sin, 0, 2, n_iter=10000))
-        self.assertEqual(integrate_async(math.sin, 0, 2, n_jobs=4, n_iter=100000),
-                         integrate(math.sin, 0, 2, n_iter=100000))
-        self.assertEqual(integrate_async(math.sin, 1, 20, n_jobs=4, n_iter=1000),
-                         integrate(math.sin, 1, 20, n_iter=1000))
-        self.assertEqual(integrate_async(math.sin, 1, 20, n_jobs=4, n_iter=10000),
-                         integrate(math.sin, 1, 20, n_iter=10000))
-        self.assertEqual(integrate_async(math.sin, 1, 20, n_jobs=4, n_iter=100000),
-                         integrate(math.sin, 1, 20, n_iter=100000))
-
-    def test_cos_async4(self):
-        self.assertEqual(integrate_async(math.cos, 0, 2, n_jobs=4, n_iter=1000),
-                         integrate(math.cos, 0, 2, n_iter=1000))
-        self.assertEqual(integrate_async(math.cos, 0, 2, n_jobs=4, n_iter=10000),
-                         integrate(math.cos, 0, 2, n_iter=10000))
-        self.assertEqual(integrate_async(math.cos, 0, 2, n_jobs=4, n_iter=100000),
-                         integrate(math.cos, 0, 2, n_iter=100000))
-        self.assertEqual(integrate_async(math.cos, 1, 20, n_jobs=4, n_iter=1000),
-                         integrate(math.cos, 1, 20, n_iter=1000))
-        self.assertEqual(integrate_async(math.cos, 1, 20, n_jobs=4, n_iter=10000),
-                         integrate(math.cos, 1, 20, n_iter=10000))
-        self.assertEqual(integrate_async(math.cos, 1, 20, n_jobs=4, n_iter=100000),
-                         integrate(math.cos, 1, 20, n_iter=100000))
+def test_sin_async2():
+    assert integrate_async(math.sin, 0, 2, n_iter=1000) == integrate(math.cos, 0, 2, n_iter=1000)
+    assert integrate_async(math.sin, 0, 2, n_iter=10000) == integrate(math.cos, 0, 2, n_iter=10000)
+    assert integrate_async(math.sin, 0, 2, n_iter=100000) == integrate(math.cos, 0, 2, n_iter=100000)
+    assert integrate_async(math.sin, 1, 20, n_iter=1000) == integrate(math.cos, 1, 20, n_iter=1000)
+    assert integrate_async(math.sin, 1, 20, n_iter=10000) == integrate(math.cos, 1, 20, n_iter=10000)
+    assert integrate_async(math.sin, 1, 20, n_iter=100000) == integrate(math.cos, 1, 20, n_iter=100000)
 
 
-if __name__ == '__main__':
-    unittest.main(verbosity=1)
+def test_cos_async2():
+    assert integrate_async(math.cos, 0, 2, n_iter=1000) == integrate(math.cos, 0, 2, n_iter=1000)
+    assert integrate_async(math.cos, 0, 2, n_iter=10000) == integrate(math.cos, 0, 2, n_iter=10000)
+    assert integrate_async(math.cos, 0, 2, n_iter=100000) == integrate(math.cos, 0, 2, n_iter=100000)
+    assert integrate_async(math.cos, 1, 20, n_iter=1000) == integrate(math.cos, 1, 20, n_iter=1000)
+    assert integrate_async(math.cos, 1, 20, n_iter=10000) == integrate(math.cos, 1, 20, n_iter=10000)
+    assert integrate_async(math.cos, 1, 20, n_iter=100000) == integrate(math.cos, 1, 20, n_iter=100000)
+
+
+def test_sin_async4():
+    assert integrate_async(math.sin, 0, 2, n_jobs=4, n_iter=1000) == integrate(math.cos, 0, 2, n_iter=1000)
+    assert integrate_async(math.sin, 0, 2, n_jobs=4, n_iter=10000) == integrate(math.cos, 0, 2, n_iter=10000)
+    assert integrate_async(math.sin, 0, 2, n_jobs=4, n_iter=100000) == integrate(math.cos, 0, 2, n_iter=100000)
+    assert integrate_async(math.sin, 1, 20, n_jobs=4, n_iter=1000) == integrate(math.cos, 1, 20, n_iter=1000)
+    assert integrate_async(math.sin, 1, 20, n_jobs=4, n_iter=10000) == integrate(math.cos, 1, 20, n_iter=10000)
+    assert integrate_async(math.sin, 1, 20, n_jobs=4, n_iter=100000) == integrate(math.cos, 1, 20, n_iter=100000)
+
+
+def test_cos_async4():
+    assert integrate_async(math.cos, 0, 2, n_jobs=4, n_iter=1000) == integrate(math.cos, 0, 2, n_iter=1000)
+    assert integrate_async(math.cos, 0, 2, n_jobs=4, n_iter=10000) == integrate(math.cos, 0, 2, n_iter=10000)
+    assert integrate_async(math.cos, 0, 2, n_jobs=4, n_iter=100000) == integrate(math.cos, 0, 2, n_iter=100000)
+    assert integrate_async(math.cos, 1, 20, n_jobs=4, n_iter=1000) == integrate(math.cos, 1, 20, n_iter=1000)
+    assert integrate_async(math.cos, 1, 20, n_jobs=4, n_iter=10000) == integrate(math.cos, 1, 20, n_iter=10000)
+    assert integrate_async(math.cos, 1, 20, n_jobs=4, n_iter=100000) == integrate(math.cos, 1, 20, n_iter=100000)
